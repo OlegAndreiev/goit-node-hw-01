76edcdcbf1627dd311499622b9f2011087193d41
Oleg Andreiev <mr.olegandreev@gmail.com>
2023-02-18 01:26:13 +0200

    Working

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+    case "get":
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await addContact({ name, email, phone });
+      return console.log(newContact);
+    case "remove":
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
+    default:
+      return console.log("Unknown action");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
